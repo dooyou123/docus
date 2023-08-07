6d52b86e1a5b8d44f76732101252a025975b11d6
dooyou123 <112735161+dooyou123@users.noreply.github.com>
2023-08-07 19:18:09 +0900

    124

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,13 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
+    '@nuxt/devtools',
+
+    '@nuxtjs/axios'
+
   ]
+  axios: {
+    baseURL: 'https://api.example.com'
+    // baseURL 설정 (예: 'https://api.example.com')
+  },
 })
---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <LoginForm @submit="handleLogin" />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from '@nuxtjs/composition-api';
+  import LoginForm from '~/components/LoginForm.vue';
+  
+  export default defineComponent({
+    components: {
+      LoginForm,
+    },
+    methods: {
+      handleLogin(credentials) {
+        // 로그인 요청을 전송하고 결과를 처리하세요.
+        console.log('Login information:', credentials);
+      },
+    },
+  });
+  </script>
+  
