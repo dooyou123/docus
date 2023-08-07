301a0ccdef6a181d321b5b2118fd69467953e67b
dooyou123 <112735161+dooyou123@users.noreply.github.com>
2023-08-07 15:03:15 +0900

    Create login.vue

---
+++ components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="loginWithGoogle">Login with Google</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup(_, { root }) {
+    const loginWithGoogle = () => {
+      root.$auth.loginWith('google')
+    }
+
+    return { loginWithGoogle }
+  },
+})
+</script>
