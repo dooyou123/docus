9228d2c7560f201342b56b4fa1753d0cce7ba132
dooyou123 <112735161+dooyou123@users.noreply.github.com>
2023-08-07 22:33:32 +0900

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
-
+  middleware: [
+    'hide-main-header',
+    // 다른 미들웨어가 있다면 여기에 추가
+  ],
 
   modules: [
     // https://github.com/nuxt-modules/plausible
