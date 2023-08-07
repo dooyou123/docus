b574863401c196f49d6a50a0dbb5bbfdb64a4576
dooyou123 <112735161+dooyou123@users.noreply.github.com>
2023-08-07 19:04:29 +0900

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
+  
+  auth: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 
   modules: [
     // https://github.com/nuxt-modules/plausible
